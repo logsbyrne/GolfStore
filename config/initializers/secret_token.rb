7fd36300f30032c21491f6537bc0341845bb6a69
mbyrne1970@gmail.com <wafuser@wafuser-VirtualBox.(none)>
2012-11-21 20:20:14 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Golfstore::Application.config.secret_token = 'bc8e28eba67f203ee56a654e3683c34a61cebd4d54a7767c1707db3a227f74d3c7c6a1043f0c3fccad62b8d1d3af60c9b45a836bc68d7848f42ca9d8f86355df'
